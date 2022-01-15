0143e8b9b9f1f501025ae9570e5ed414358046f4
Jackson E <lavaskin@gmail.com>
2022-01-14 18:54:22 -0800

    More minor refactors for the rebrand.

--- app/src/androidTest/java/lavaskin/app/ll/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/lavaskin/app/ll/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.cs492final", appContext.getPackageName());
+        assertEquals("lavaskin.app.ll", appContext.getPackageName());
     }
 }
